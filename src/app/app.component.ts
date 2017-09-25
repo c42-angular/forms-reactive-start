2d6d030a4249349932d519e6f0664cbf45d5d457
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2017-09-25 07:03:08 +0100

    Form created using reactive approach and submit works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(),
+      'email': new FormControl(),
+      'gender': new FormControl('female')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
 }
